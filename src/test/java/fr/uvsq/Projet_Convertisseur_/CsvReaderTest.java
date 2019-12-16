16a16f4e0b72bf21ced333208c9e65e4f4ab57fd
Nalsolniel <thlapegue88@gmail.com>
2019-12-16 12:37:35 +0100

    normalement la fin de csv reader

--- src/test/java/fr/uvsq/Projet_Convertisseur_/CsvReaderTest.java
+++ src/test/java/fr/uvsq/Projet_Convertisseur_/CsvReaderTest.java
@@ -6,7 +6,11 @@ package fr.uvsq.Projet_Convertisseur_;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.SingleSelectionModel;
 
 import org.junit.Test;
 /**
@@ -95,7 +99,254 @@ public class CsvReaderTest {
 		//ecrireMotPareilActualiserMatriceBoolean
 		//public void ecrireMotPareilActualiserMatriceBoolean(String[] matriceATraitee,boolean[] matriceTraitee,FileWriter write)
 		
+		@Test
+		public final void TestTrueecrireMotPareilActualiserMatriceBoolean() {
+			csvReader c = new csvReader();
+			boolean[] tab= new boolean[10];
+			
+			
+			String[] MatriceElem= new String[10];
+			for (int i=0;i<10;i++) {
+				tab[i]=false;
+			}
+			for (int i=0;i<10;i++) {
+				MatriceElem[i]=null;
+			}
+			MatriceElem[1]="tel_mobile";
+			MatriceElem[2]="tel_fax_perso";
+			MatriceElem[3]="tel_fixe";
+
+			File f = new File("conf.txt");
+			try 
+			{
+				f.createNewFile();
+				FileWriter write = new FileWriter(f);
+			c.ecrireMotPareilActualiserMatriceBoolean(MatriceElem,tab,write);
+			
+			write.close();
+			}
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+		
+			if(tab[1]==true) {
+				if(tab[2]==true) {
+					assertEquals(tab[3],true);
+					
+				}
+			}
+			else {
+				fail("test echoué"); 
+			}
+		}
 		
 		
+		//  genConfFile pense impossible
+		//public void genConfFile(String[] header)
+
+		
+		// peutEcrire
+		//public boolean peutEcrire(String[] blackList,String mot)
+		@Test
+		public final void TestTruepeutEcrire() {
+			csvReader c = new csvReader();
+			String mot =new String();
+			mot="nom";
+			boolean[] tab= new boolean[10];
+			String[] MatriceElem= new String[10];
+			for (int i=0;i<10;i++) {
+				tab[i]=false;
+			}
+			for (int i=0;i<10;i++) {
+				MatriceElem[i]=null;
+			}
+			MatriceElem[1]="tel_mobile";
+			MatriceElem[2]="tel_fax_perso";
+			MatriceElem[4]="tel_fixe";
+			assertEquals(c.peutEcrire(MatriceElem, mot),true);
+			
+		}
+		
+		@Test
+		public final void TestfalsepeutEcrire() {
+			csvReader c = new csvReader();
+			String mot =new String();
+			mot="tel_fixe";
+			boolean[] tab= new boolean[10];
+			String[] MatriceElem= new String[10];
+			for (int i=0;i<10;i++) {
+				tab[i]=false;
+			}
+			for (int i=0;i<10;i++) {
+				MatriceElem[i]=null;
+			}
+			MatriceElem[1]="tel_mobile";
+			MatriceElem[2]="tel_fax_perso";
+			MatriceElem[4]="tel_fixe";
+			assertEquals(c.peutEcrire(MatriceElem, mot),false);
+			
+		}
+		// reader impossible a tester
+		//public void reader()
+		
+		// selectTab
+		//public String[] selectTab(String[] matriceATraitee,boolean[] matriceTraitee)
+		
+		@Test
+		public final void TestTrueselectTab() {
+			csvReader c = new csvReader();
+			int ok=1;
+			boolean[] tab= new boolean[10];
+			String[] MatriceElem= new String[10];
+			String[] test= new String[10];
+			for (int i=0;i<10;i++) {
+				tab[i]=false;
+			}
+			for (int i=0;i<10;i++) {
+				MatriceElem[i]=null;
+			}
+			MatriceElem[1]="tel_mobile";
+			MatriceElem[2]="tel_fax_perso";
+			MatriceElem[4]="tel_fixe";
+			MatriceElem[3]="tel_fixe";
+			MatriceElem[5]="tel_fixe";
+			MatriceElem[6]="tel_fixe";
+			MatriceElem[7]="tel_fixe";
+			MatriceElem[8]="tel_fixe";
+			MatriceElem[9]="tel_fixe";
+			MatriceElem[0]="Nom";
+			//System.out.println("bonjour");
+			test= c.selectTab(MatriceElem,tab);
+			//System.out.println("bouuuuuuuuuuuunjour");
+			for(int i=9;i>0;i--) {
+			
+				if (test[i]!=null) {
+					ok=0;
+				}
+			}
+			if(ok==1) {assertNotEquals(MatriceElem[0],null);}
+			
+		}
+		
+		@Test
+		public final void TestFalseselectTab() {
+			csvReader c = new csvReader();
+			int ok=1;
+			boolean[] tab= new boolean[10];
+			String[] MatriceElem= new String[10];
+			String[] test= new String[10];
+			for (int i=0;i<10;i++) {
+				tab[i]=false;
+			}
+			tab[1]=true;
+			for (int i=0;i<10;i++) {
+				MatriceElem[i]=null;
+			}
+			MatriceElem[1]="tel_mobile";
+			MatriceElem[2]="tel_fax_perso";
+			MatriceElem[4]="tel_fixe";
+			MatriceElem[3]="tel_fixe";
+			MatriceElem[5]="tel_fixe";
+			MatriceElem[6]="tel_fixe";
+			MatriceElem[7]="tel_fixe";
+			MatriceElem[8]="tel_fixe";
+			MatriceElem[9]="tel_fixe";
+			MatriceElem[0]="Nom";
+			//System.out.println("bonjour");
+			test= c.selectTab(MatriceElem,tab);
+			//System.out.println("bouuuuuuuuuuuunjour");
+			for(int i=9;i>0;i--) {
+			
+				if (test[i]==null) {
+					ok=0;
+				}
+			}
+			if(ok==1) {assertEquals(MatriceElem[0],null);}
+			
+		}
+		
+		//tailleMaxElementTableau 
+		//public int tailleMaxElementTableau(String[] matriceATraitee) 
+		@Test
+		public final void TestTruetailleMaxElementTableau() {
+			csvReader c = new csvReader();
+			int ok=1;
+			
+			String[] MatriceElem= new String[10];
+		
+			
+			for (int i=0;i<10;i++) {
+				MatriceElem[i]=null;
+			}
+			MatriceElem[1]="tel_mobile";
+			MatriceElem[2]="tel_fax_perso";
+			MatriceElem[4]="tel_fixe";
+			MatriceElem[3]="tel_fixe";
+			MatriceElem[5]="tel_2_3_4";
+			MatriceElem[6]="tel_fixe";
+			MatriceElem[7]="tel_fixe";
+			MatriceElem[8]="tel_fixe";
+			MatriceElem[9]="tel_fixe";
+			
+	 {assertEquals(c.tailleMaxElementTableau(MatriceElem),4);}
+			
+		}
+		//  verifMot
+		//public String[] verifMot(String[] transition)
+		
+		@Test
+		public final void TestTrueVerifmot() {
+			csvReader c = new csvReader();
+			String[] mot =new String[2];
+			mot[0]="tel";
+			mot[1]="fixe";
+			int nb=0;
+			String[] MatriceElem= new String[10];
+			
+			for (int i=0;i<10;i++) {
+				MatriceElem[i]=null;
+			}
+		
+			c.verifMot(mot);
+			MatriceElem=c.verifMot(mot);
+			for (int i=9;i>-1;i--) {
+				//MatriceElem[i]=null;
+				//System.out.println(MatriceElem[i]);
+				if(MatriceElem[i]!=null) {
+					nb+=1;
+				}
+			}
+			System.out.println(nb);
+			assertEquals(nb,2);
+		}
+		@Test
+		public final void Test2TrueVerifmot() {
+			csvReader c = new csvReader();
+			String[] mot =new String[2];
+			mot[0]="tel";
+			mot[1]="fixe";
+			int nb=0;
+			String[] MatriceElem= new String[10];
+			
+			for (int i=0;i<10;i++) {
+				MatriceElem[i]=null;
+			}
+		
+			MatriceElem=c.verifMot(mot);
+			mot[0]="tel";
+			mot[1]="mobile";
+			
+			MatriceElem=c.verifMot(mot);
+			for (int i=9;i>-1;i--) {
+				//MatriceElem[i]=null;
+				System.out.println(MatriceElem[i]);
+				if(MatriceElem[i]!=null) {
+					nb+=1;
+				}
+			}
+			System.out.println(nb);
+			assertEquals(nb,3);
+		}
 		
 }
